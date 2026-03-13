6a37b2477e936facd77d0b63ac415a4c620d51f7
Ryan Yeung <ryeung@connect.kellettschool.com>
2026-03-13 07:04:06 +0000

    13 Mar 2026, 15:04

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 ### Personal Programming Project - Ryan Yeung
 
-
 if __name__ == "__main__":
     input("""Hello, Please choose an option: 
           1. Settings 
           2. Blackjack
           3. Poker
           4. Roulette 
-          5. Exit""")
-    
+          5. Exit
+          
+          """)
     
