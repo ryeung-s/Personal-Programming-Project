d654718d935993e67965b4123f107a99a227bab0
Ryan Yeung <ryeung@connect.kellettschool.com>
2026-03-19 04:27:43 +0000

    19 Mar 2026, 12:27

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ def display_logo():
                 outputlines= []
                 for line in list_logo:
                     #for i in range(len(line)):
-                    if index > 40 and index <= (len(list_logo[0])-counter):
+                    if index > 40 and index <= (len(list_logo[0])):
                         outputlines.append((" "*(index-counter)) + line[index-counter:index])
                         pass
-                    elif index >= (len(list_logo[0])-counter):
+                    elif index >= (len(list_logo[0])):
                         outputlines.append((" "*(index-counter)) + line[index-counter:])
                         
                         pass
