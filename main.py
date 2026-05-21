ff7ce4f96783b82729aba1401989c64b8759ec38
Ryan Yeung <ryeung@connect.kellettschool.com>
2026-05-21 06:56:10 +0000

    21 May 2026, 14:56

--- main.py
+++ main.py
@@ -225,8 +225,8 @@ def create_players(num_players):
 def printplayerinfo(players):
     spaces = 10
     
-    names_line = (" "*(spaces)).join([f"Player {i+1}: {p.name}" for i, p in enumerate(players)])
-    hands_line = (" "*(spaces-4)).join([f"Hand: {p.hand}" for p in players])
+    names_line = (" "*(spaces-len(p.name for p in players))).join([f"Player {i+1}: {p.name}" for i, p in enumerate(players)])
+    hands_line = (" "*(spaces-4)).join([f"Hand: {" ".join(p.hand)}" for p in players])
     money_line = (" "*(spaces+4)).join([f"$: {p.money}" for p in players])
     
     print(names_line)
