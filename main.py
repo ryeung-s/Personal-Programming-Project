443f35536e0040a3857718afa5649b893d6821f4
Ryan Yeung <ryeung@connect.kellettschool.com>
2026-06-02 03:43:42 +0000

    2 Jun 2026, 11:43

--- main.py
+++ main.py
@@ -238,7 +238,7 @@ def printplayerinfo(players):
     
     for i in range(len(finalhands[0])):
             line = "   ".join(hand[i] for hand in finalhands if i < len(hand)) 
-            print(line)
+    print(line)
     #print(hand[0])
     #print(" ".join(player_hands))
     #print("".join(player_hands), end="\r")
