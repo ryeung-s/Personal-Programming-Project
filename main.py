3381811a89b3e2e37cf099cecdb3ddb8a976801c
Ryan Yeung <ryeung@connect.kellettschool.com>
2026-06-14 13:39:32 +0000

    Jun 14, 2026, 9:39 PM

--- main.py
+++ main.py
@@ -204,11 +204,11 @@ def PlayBlackjack():
     bet = 0
     create_players(num_players)
     for c, player in enumerate(players):
-        if player.name == "Dealer":
-            print(f"Player {c+1}: {player.name}")
-            print(f"Money: {player.money}")
-            betamnt = input("Bet amount: ")
-            betamnt = validinput(betamnt, "option.isdigit() and int(betamnt) <= player.money")
+
+        print(f"Player {c+1}: {player.name}")
+        print(f"Money: {player.money}")
+        betamnt = input("Bet amount: ")
+        betamnt = validinput(betamnt, "option.isdigit() and int(betamnt) <= player.money")
         player.bet = int(betamnt)      
         for i in range(2):
             print(CURSOR_HOME)
