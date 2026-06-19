16f853f9412d7285173d3b805708340506c04e75
Ryan Yeung <ryeung@connect.kellettschool.com>
2026-06-19 09:12:49 +0000

    Jun 19, 2026, 5:12 PM

--- main.py
+++ main.py
@@ -372,6 +372,7 @@ def BJturn(player):
             for b in range(player.bet):
                 player.bet -= 1
                 printplayerinfo(players)
+                sleep(0.05)
             sleep(0.2)
             # if at21:
             #     print("21!!")
