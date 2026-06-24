f30741e16159a85dc4a48d3a0811c48bef020553
Ryan Yeung <ryeung@connect.kellettschool.com>
2026-06-24 01:36:42 +0000

    24 Jun 2026, 09:36

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 palindrome_list = []
+count = 0
 for i in range(100, 999):
-    palindrome_list.append(str(i))
-
+    palindrome = (str(i))
+    palindrome += palindrome[::-1]
+    palindrome_list.append(palindrome)
+for num in palindrome_list:
+    if int(num) % 9 == 0:
+        print(num)
+        count += 1
+        print(count)
